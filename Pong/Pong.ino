bff1acf41877acc6fde2435426a7d5d0247707c4
Pedro Henrique Esteves Sarmento <pedrohenriquesarmento@gmail.com>
2025-07-12 12:18:59 -0300

    0.1.0
    Implementando o reconhecimento dos botões e com o toggle

--- Pong/Pong.ino
+++ Pong/Pong.ino
@@ -1,6 +1,5 @@
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
-#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
 
 // SDA no pino 23
@@ -11,7 +10,6 @@
 #define TFT_BACKLIGHT  22 // Display backlight pin
 
 #define SerialDebugging true
-
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
@@ -45,91 +43,126 @@ volatile bool   isButtonPressedDir = false;
 
 bool testeCor = false;
 
+int esquerdo, direito = 0;
+int tempBotaoEsq = 0;
+int tempBotaoDir = 0;
+
 // interrupt service routine
 void senseButtonPressedEsq() {
-    if (!isButtonPressedEsq) {
-        isButtonPressedEsq = true;
-    }
+  if (!isButtonPressedEsq) {
+      isButtonPressedEsq = true;
+  }
 }
 void senseButtonPressedDir() {
-    if (!isButtonPressedDir) {
-        isButtonPressedDir = true;
-    }
+  if (!isButtonPressedDir) {
+      isButtonPressedDir = true;
+  }
 }
 
 void mudaCor(){
   if(testeCor){
     tft.fillScreen(Display_Color_Black);
+    tft.setTextColor(Display_Color_White);
+    tft.setCursor(0, 0);
+    tft.print("Botao Esquerdo: ");
+    tft.print(String(esquerdo));
+    tft.setCursor(0, 20);
+    tft.print("Botao Direito: ");
+    tft.print(String(direito));
     testeCor = false;
   }
   else{
-    tft.fillScreen(Display_Color_White);
+    tft.fillScreen(Display_Color_Black);
+    tft.setTextColor(Display_Color_White);
+    // tft.fillScreen(Display_Color_White);
+    // tft.setTextColor(Display_Color_Black);
+    tft.setCursor(0, 0);
+    tft.print("Botao Esquerdo: ");
+    tft.print(String(esquerdo));
+    tft.setCursor(0, 20);
+    tft.print("Botao Direito: ");
+    tft.print(String(direito));
     testeCor = true;
   }
 }
 
+void botoes(){
+  // has the button been pressed?
+  if (isButtonPressedEsq) {
+    // confirm button handled
+    isButtonPressedEsq = false;
+    esquerdo++;
+    tempBotaoEsq = millis();
+    mudaCor();
+  }
+  else if(!isButtonPressedEsq && digitalRead(botaoEsq) == HIGH && millis() - tempBotaoEsq >= 450){
+    // Serial.println("Ainda está apertado o botaoEsq");
+    esquerdo++;
+    mudaCor();
+  }
+
+  if(isButtonPressedDir) {
+    // confirm button handled
+    isButtonPressedDir = false;
+    direito++;
+    tempBotaoDir = millis();
+    mudaCor();
+  }
+  else if(!isButtonPressedDir && digitalRead(botaoDir) == HIGH && millis() - tempBotaoDir >= 450){
+    // Serial.println("Ainda está apertado o botaoEsq");
+    direito++;
+    mudaCor();
+  }
+}
+
 void setup() {
+  pinMode(botaoEsq, INPUT_PULLUP);
+  pinMode(botaoDir, INPUT_PULLUP);
 
-    // button press pulls pin LOW so configure HIGH
-    pinMode(botaoEsq, INPUT_PULLUP);
-    pinMode(botaoDir, INPUT_PULLUP);
+  // Usa interrupção ou seja o microcontrolador vai estar verificando esse pino a todo momento
+  // Quando ele vê que foi acionado ele pausa o código principal para executar o codigo atrelado a esse pino
+  attachInterrupt(digitalPinToInterrupt(botaoEsq), senseButtonPressedEsq, HIGH);
+  attachInterrupt(digitalPinToInterrupt(botaoDir), senseButtonPressedDir, HIGH);
 
-    // Usa interrupção ou seja o microcontrolador vai estar verificando esse pino a todo momento
-    // Quando ele vê que foi acionado ele pausa o código principal para executar o codigo atrelado a esse pino
-    attachInterrupt(digitalPinToInterrupt(botaoEsq), senseButtonPressedEsq, FALLING);
-    attachInterrupt(digitalPinToInterrupt(botaoDir), senseButtonPressedDir, FALLING);
+  #if (SerialDebugging)
+  Serial.begin(115200); while (!Serial); Serial.println();
+  #endif
 
-    #if (SerialDebugging)
-    Serial.begin(115200); while (!Serial); Serial.println();
-    #endif
+  // settling time
+  delay(250);
 
-    // settling time
-    delay(250);
+  // ignore any power-on-reboot garbage
+  isButtonPressedEsq = false;
+  isButtonPressedDir = false;
 
-    // ignore any power-on-reboot garbage
-    isButtonPressedEsq = isButtonPressedDir = false;
+  tft.initR(INITR_144GREENTAB);
 
-    #ifdef ADAFRUIT_HALLOWING
-      // HalloWing is a special case. It uses a ST7735R display just like the
-      // breakout board, but the orientation and backlight control are different.
-      tft.init(128, 128);
 
-      tft.setRotation(0);  // 0, 1, 2 ou 3 para ajustar a rotação
+  tft.setRotation(0);  // 0, 1, 2 ou 3 para ajustar a rotação
 
-        // Initialize HalloWing-oriented screen
-      pinMode(TFT_BACKLIGHT, OUTPUT);
-      digitalWrite(TFT_BACKLIGHT, HIGH); // Backlight on
-    #else
-      // Use this initializer if using a 1.8" TFT screen:
-      tft.initR(INITR_144GREENTAB);
-    #endif
+  // Initialize HalloWing-oriented screen
+  pinMode(TFT_BACKLIGHT, OUTPUT);
+  digitalWrite(TFT_BACKLIGHT, HIGH); // Backlight on
 
-    // initialise the display
-    tft.setFont();
-    tft.fillScreen(Display_Backround_Color);
-    tft.setTextColor(Display_Text_Color);
-    tft.setTextSize(1);
+  // initialise the display
+  tft.setFont();
+  tft.fillScreen(Display_Backround_Color);
+  tft.setTextColor(Display_Text_Color);
+  tft.setTextSize(1);
+  tft.setCursor(0, 0);
+  tft.print("Botao Esquerdo: ");
+  tft.print(String(esquerdo));
+  tft.setCursor(0, 20);
+  tft.print("Botao Direito: ");
+  tft.print(String(direito));
 
-    // the display is now on
-    isDisplayVisible = true;
-    tft.enableDisplay(isDisplayVisible);
-
+  // the display is now on
+  isDisplayVisible = true;
+  tft.enableDisplay(isDisplayVisible);
 }
 
 void loop() {
-    // has the button been pressed?
-    if (isButtonPressedEsq) {
-        // confirm button handled
-        isButtonPressedEsq = false; 
-        mudaCor();
-    }
-    if(isButtonPressedDir) {
-        // confirm button handled
-        isButtonPressedDir = false;
-        mudaCor();
-    }
-    // no need to be in too much of a hurry
-    delay(100);
-   
+  botoes();
+  delay(100);
 }
 
